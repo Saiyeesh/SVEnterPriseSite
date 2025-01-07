7f3c65c7041add17057fc3230a2656181baeb41d
Saiyeesh <saiyeesh7941@gmail.com>
2025-01-07 23:23:17 +0530

    maps change

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -71,7 +71,7 @@ function Contact() {
                 <button type="submit">Send</button>
             </form>
             <div className="secoundPart2">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42772.77845288112!2d77.89779330279096!3d17.37824330635356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99f2bc5c223d%3A0xba19fe69218267c7!2sSV-ENTERPRISES%20Non-woven%20Bags%20%26%20Printing!5e0!3m2!1sen!2sin!4v1736269323319!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42772.77845288112!2d77.89779330279096!3d17.37824330635356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99f2bc5c223d%3A0xba19fe69218267c7!2sSV-ENTERPRISES%20Non-woven%20Bags%20%26%20Printing!5e0!3m2!1sen!2sin!4v1736269323319!5m2!1sen!2sin" width="500" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="location"></iframe>
             </div>
         </div>
         <p>{status}</p>
